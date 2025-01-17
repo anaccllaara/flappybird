132da9c034d947721f6aa2f6ccd2cf150a9df1b1
anaccllaara <anaccllaara@gmail.com>
2025-01-17 10:11:54 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -4,19 +4,8 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('#game-canvas');
 const contexto = canvas.getContext('2d');
 
-function loop(){
-
-    contexto.drawImage(
-        sprites,
-        0, 0,
-        35, 25,
-        10, 50,
-        35, 25);
-        
-        requestAnimationFrame(loop);
-    }
-        
-    loop();
+    contexto.fillStyle = '#70c5ce';
+    contexto.fillRect(0,0, canvas.width, canvas.height)
 
 const flappyBird = {
     spriteX: 0,
@@ -35,3 +24,67 @@ const flappyBird = {
         );
     }
 }
+
+const imagemDeFundo = {
+    spriteX: 390,
+    spriteY: 3,
+    largura: 275,
+    altura: 201,
+    x: 0,
+    y: 279,
+        desenha() {
+        contexto.drawImage(
+            sprites,
+            imagemDeFundo.spriteX, imagemDeFundo.spriteY,
+            imagemDeFundo.largura, imagemDeFundo.altura,
+            imagemDeFundo.x, imagemDeFundo.y,
+            imagemDeFundo.largura, imagemDeFundo.altura,
+        );
+        contexto.drawImage(
+            sprites,
+            imagemDeFundo.spriteX, imagemDeFundo.spriteY,
+            imagemDeFundo.largura, imagemDeFundo.altura,
+            imagemDeFundo.x+imagemDeFundo.largura, imagemDeFundo.y,
+            imagemDeFundo.largura, imagemDeFundo.altura,
+        );
+    }
+}
+
+const chao = {
+    spriteX: 1,
+    spriteY: 610,
+    largura: 219,
+    altura: 109,
+    x: 0,
+    y: 372,
+        desenha() {
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x+chao.largura, chao.y,
+            chao.largura, chao.altura,
+        );
+    }
+}
+
+function loop(){
+
+    flappyBird.desenha();
+    imagemDeFundo.desenha();
+    chao.desenha();
+        
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+//width largura x
+//height altura y
